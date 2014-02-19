e4f262a3160c166ef4bdc133df54f5ccc348a1f9
Kay Strobach <github@kay-strobach.de>
2014-02-19 17:50:24 +0100

    update apt order

--- serverdata/provision/manifests/default.pp
+++ serverdata/provision/manifests/default.pp
@@ -21,12 +21,12 @@ package { "mysql-server":               ensure  => "latest", require  => Exec['a
 package { "mysql-client":               ensure  => "latest", require  => Exec['apt-get update']}
 
 package { "apache2":                    ensure  => "latest", require  => Exec['apt-get update']}
-package { "libapache2-mod-php5":        ensure  => "latest", require  => Exec['apt-get update']}
-package { "php5":                       ensure  => "latest", require  => Exec['apt-get update']}
-package { "php5-gd":                    ensure  => "latest", require  => Exec['apt-get update']}
-package { "php5-curl":                  ensure  => "latest", require  => Exec['apt-get update']}
-package { "php5-mysql":                 ensure  => "latest", require  => Exec['apt-get update']}
-package { "phpmyadmin":                 ensure  => "latest", require  => Exec['apt-get update']}
+package { "libapache2-mod-php5":        ensure  => "latest", require  => Package['php5']}
+package { "php5":                       ensure  => "latest", require  => Package['apache2']}
+package { "php5-gd":                    ensure  => "latest", require  => Package['php5']}
+package { "php5-curl":                  ensure  => "latest", require  => Package['php5']}
+package { "php5-mysql":                 ensure  => "latest", require  => Package['php5']}
+package { "phpmyadmin":                 ensure  => "latest", require  => Package['php5']}
 
 
 
@@ -72,7 +72,7 @@ file { "/etc/php5/cli/php.ini":
 file { "/etc/php5/apache2/php.ini":
   ensure  => "link",
   target  => "/vagrant/serverdata/etc/php5/apache2/php.ini",
-  require => Package["apache2"],
+  require => Package["php5"],
   notify  => Service["apache2"],
   force => true,
 }
