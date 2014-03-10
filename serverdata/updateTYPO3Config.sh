4f81937e1bf2bc057f2ca28cca347107e667158f
Kay Strobach <github@kay-strobach.de>
2014-03-10 14:49:01 +0100

    Add latest sql dump

---
+++ serverdata/updateTYPO3Config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp /var/www/typo3conf/PackageStates.php      /vagrant/project/typo3conf/PackageStates.php
+cp /var/www/typo3conf/LocalConfiguration.php /vagrant/project/typo3conf/LocalConfiguration.php
